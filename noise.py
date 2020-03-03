ad8e34c8e587b66f270ea73b2e73ab63fba65d86
David Kadish <david.kadish@gmail.com>
2020-03-03 13:25:27 +0100

    Code for producing white noise

--- noise.py
+++ noise.py
@@ -1,9 +1,11 @@
+import logging
 from collections import Iterable
 
 import numpy as np
 
 N_CHANNELS = 9
 
+logger = logging.getLogger('noise')
 
 class Noise(Iterable):
     """Outputs a constant noise for across all generations and individuals."""
@@ -38,6 +40,43 @@ class GenerationStepNoise(Noise):
 
         if self.current_generation >= self.start_generation:
             return super().__next__()
+        else:
+            return np.zeros(N_CHANNELS)
+
+    def generation(self):
+        self.current_generation += 1
+
+
+class WhiteNoise(GenerationStepNoise):
+    """Noise that appears suddenly one generation."""
+
+    def __init__(self, levels, channels, generation: int = 0, random_seed: int = None) -> None:
+        """
+
+        :param levels: Levels sets the max value for the noise, which ranges from [0,level]
+        :param channels:
+        :param generation:
+        :param random_seed:
+        """
+        super().__init__(levels, channels, generation)
+
+        self.current_generation = 0
+        self.start_generation = generation
+
+        self.random = np.random
+        if random_seed is not None:
+            logging.debug('Setting random seed to {}'.format(random_seed))
+            self.random = np.random.RandomState(random_seed)
+
+        self.n_channels = type(channels) == int and 1 or len(channels)
+
+    def __next__(self):
+
+        if self.current_generation >= self.start_generation:
+            r = self.random.random(self.n_channels) * np.array(self.levels)
+            noise = np.zeros(N_CHANNELS)
+            noise[self.channels] += r
+            return noise
         else:
             return np.zeros(N_CHANNELS)
 
--- tests/test_noise.py
+++ tests/test_noise.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-from noise import Noise, GenerationStepNoise
+from noise import Noise, GenerationStepNoise, WhiteNoise
 
 
 class TestNoise(unittest.TestCase):
@@ -52,6 +52,59 @@ class TestGenerationNoise(unittest.TestCase):
         for _ in range(6):
             np.testing.assert_equal(next(gn3), np.array([0, 0, 2.0, 0, 0, 0, 0, 0, 4.0]))
             gn3.generation()
+
+
+class TestWhiteNoise(unittest.TestCase):
+
+    def test_noise(self):
+
+        # Zero noise from the first generation
+        r = np.random.RandomState(0)
+        wn0 = WhiteNoise(0.0, 0, random_seed=0)
+        for _ in range(10):
+            n = next(wn0)
+            np.testing.assert_equal(n, r.random(9) * 0.0)
+            np.testing.assert_equal(n, np.zeros(9))
+            wn0.generation()
+
+        # Random [0,1] noise from the first generation on channel 0
+        n1 = Noise(1.0, 0)
+        wn1 = WhiteNoise(1.0, 0, random_seed=0)
+        r = np.random.RandomState(0)
+        for _ in range(10):
+            n = next(wn1)
+            compare = np.zeros(9)
+            compare[0] = r.random(1)
+            np.testing.assert_equal(n, compare)
+            wn1.generation()
+
+        # Random [0,2.0] noise from the third generation on channel 2
+        wn2 = WhiteNoise(2.0, 2, generation=2, random_seed=0)
+        r = np.random.RandomState(0)
+        for _ in range(2):
+            n = next(wn2)
+            np.testing.assert_equal(n, np.zeros(9))
+            wn2.generation()
+        for _ in range(8):
+            n = next(wn2)
+            compare = np.zeros(9)
+            compare[2] = r.random(1) * 2.0
+            np.testing.assert_equal(n, compare)
+            wn2.generation()
+
+        # Random [0,2.0] and [0,4.0] noise from the fifth generation on channels 2 and 8
+        wn3 = WhiteNoise([2.0, 4.0], [2, 8], generation=4, random_seed=0)
+        r = np.random.RandomState(0)
+        for _ in range(4):
+            n = next(wn3)
+            np.testing.assert_equal(n, np.zeros(9))
+            wn3.generation()
+        for _ in range(6):
+            n = next(wn3)
+            compare = np.zeros(9)
+            compare[[2, 8]] = r.random(2) * np.array([2.0, 4.0])
+            np.testing.assert_equal(n, compare)
+            wn3.generation()
 
 
 if __name__ == '__main__':
